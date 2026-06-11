a33a47ddf98831000cc82eb5c85abe55bb4a4264
oceana1129 <oceana1129@gmail.com>
2026-06-11 15:00:11 -0700

    code challenge #11

---
+++ 8th-kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,9 @@
+/**
+ * 8 kyu
+Function 2 - squaring an argument
+Now you have to write a function that takes 
+an argument and returns the square of it.
+ */
+const square = (num) => {
+  return num * num;
+}
